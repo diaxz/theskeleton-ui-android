4c862d772f634b10f5280a1b8126217444a299c8
diasa <diasa@A1032.asyx.com>
2017-11-23 13:56:59 +0700

    add travis support

---
+++ upload-gh-pages.sh
@@ -0,0 +1,18 @@
+mkdir $HOME/buildApk/ 
+cp -R app/build/outputs/apk/app-debug.apk $HOME/android/
+
+cd $HOME
+git config --global user.email "theskeleton@codenergic.org"
+git config --global user.name "TheSkeleton"
+
+git clone --quiet --branch=master https://diaxz:$GITHUB_API@github.com/diaxz/theskeleton-ui-android > /dev/null
+
+cd mastercp -Rf $HOME/android/* .
+
+git add -f .
+git remote rm origin
+git remote add origin https://diaxz:$GITHUB_API@github.com/diaxz/theskeleton-ui-android.git
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
+git push -fq origin master > /dev/null
+echo -e "Done\n"
